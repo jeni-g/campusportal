aa6a7d9b85c63d8be7516f271265027ac58e6296
Jeni G Hannah <jenihannah4@gmail.com>
2026-07-11 23:40:46 +0530

    fix: add 127.0.0.1 and localhost to ALLOWED_HOSTS for local dev

--- campusportal/settings.py
+++ campusportal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t^$!$5m-3!hb#(7u&%h9-v$@8m2z&z6k57jgvh7=-*0h8*!y9)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['campusportal-7q41.onrender.com']
+ALLOWED_HOSTS = ['campusportal-7q41.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
